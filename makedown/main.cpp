823e7cfe4c861079177724864e727633b4a62298
LiuZzhao <lz363567051@163.com>
2024-05-11 16:51:16 +0800

    [update]:add-block_queue.h-update-producer_consumer_mode

--- makedown/main.cpp
+++ makedown/main.cpp
@@ -1,68 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <pthread.h>
 #include <unistd.h>
-#include "locker.h" // 包含我们之前定义的locker类（互斥锁、信号量、条件变量）
+#include "block_queue.h"
 
-struct student {
-    int nu;
-    struct student* next;
-};
-
-struct student* head = NULL;
-locker mutex;
-cond m_cond;
-sem full(0);
-
-//生产者
+// 生产者线程函数
 void* producer(void* arg) {
-    int i = 0;
-    while(1) {
-        struct student* node = (struct student*)malloc(sizeof(struct student));
-        node->nu = i++;
-        printf("+++++++++++++++++生产者产生数据i = %d\n", node->nu);
-        mutex.lock();
-        //连彪插入，头插
-        node->next = head;
-        head = node;
-        mutex.unlock();
-        m_cond.signal();
+    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
+    int item = 0;
+    while (true) {
+        queue->push(item);
+        printf("+++++++++++++++++生产者产生数据i = %d\n", item);
+        item++;
         usleep((rand()%5 + 1) * 50000);
     }
-
-    return NULL;
+    return nullptr;
 }
 
-//消费者
-void* consumer(void* arg) {
-    struct student* node;
-    while(1) {
-        mutex.lock();
-        while(head == NULL) {
-            //阻塞在条件变量上
-            m_cond.wait(mutex.get());
-        }
-        //摘下头节点
-        node = head;
-        head = head->next;
-        mutex.unlock();
-        printf("-----消费者处理数据i = %d\n", node->nu);
-        free(node);
-        usleep((rand()%5 + 1) * 50000);
+// 消费者线程函数
+void* consumer(void* arg){
+    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
+    int item;
+    while (true) {
+        queue->pop(item);
+        printf("-----消费者处理数据i = %d\n", item);
     }
-    return NULL;
+    return nullptr;
 }
-
 
 int main() {
-    pthread_t prod, cons;
-    // 创建生产者和消费者线程
-    pthread_create(&prod, NULL, producer, NULL);
-    pthread_create(&cons, NULL, consumer, NULL);
-
-    // 等待线程结束
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
+    block_queue<int> queue;
+    pthread_t tid_prod, tid_cons;
+    pthread_create(&tid_prod, nullptr, producer, &queue);
+    pthread_create(&tid_cons, nullptr, consumer, &queue);
+    pthread_join(tid_prod, nullptr);
+    pthread_join(tid_cons, nullptr);
 
     return 0;
 }
