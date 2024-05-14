c083b80fb889db62e4880ca24a281bf6728b71b7
Vilaman <630556320@qq.com>
2024-05-14 16:36:45 -0700

    update

--- 20240420/一阶段项目/client/client_info.h
+++ 20240420/一阶段项目/client/client_info.h
@@ -7,7 +7,8 @@
 #ifndef CLIENT_INFO_H
 #define CLIENT_INFO_H
 
-#define CLIENT_LOGIN "CLIENT_LOGIN"           //客户端登录
+#define NORMALUSER_LOGIN "NORMALUSER_LOGIN"   //非管理员用户登录
+#define ADMIN_LOGIN "ADMIN_LOGIN"             //管理员登录登录
 #define USER_REGISTER "USER_REGISTER"         //注册用户
 #define DELETE_USER "DELETE_USER"             //删除用户
 #define CHANGE_INFO "CHANGE_INFO"             //管理员修改用户信息
@@ -18,9 +19,8 @@
 #define NORMAL_QUERYOP "NORMAL_QUERYOP"       //普通用户查看操作日志
 #define CONNECT_SUCCESS "CONNECT_SUCCESS"     //连接服务器成功
 #define PORT 8088                             //定义端口信息
-
-
-
+#define N 200        //定义客户端发送数据字节数
+#define MAXSIZE 1024 //定义接收最大字节数
 
 
 
--- 20240420/一阶段项目/client/client_main.c
+++ 20240420/一阶段项目/client/client_main.c
@@ -24,34 +24,38 @@
 User u;//员工信息结构体变量
 
 
-int main(){
-//网络套接字连接服务器
-	int client_fd=socket(AF_INET,SOCK_STREAM,0);
-	struct sockaddr_in server;
-	server.sin_family=AF_INET;
-	server.sin_port=htons(PORT);
-	server.sin_addr.s_addr=inet_add("127.0.0.1");
-	
-	if(connect(client_fd,(struct sockaddr *)&server,sizeof(server))<0){
+int main() {
+  //网络套接字连接服务器
+  int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in server;
+  server.sin_family = AF_INET;
+  server.sin_port = htons(PORT);
+  server.sin_addr.s_addr = inet_add("127.0.0.1");
 
-		perror("CONNECT()");
-		exit(-1);
+  if (connect(client_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+
+    perror("CONNECT()");
+    exit(-1);
+  }
+
+  puts("连接服务器成功");
+  bool isloginpass = false; //是否成功登录
+  bool isRun = true;        //定义外层循环
+  bool isMainmenu = true;   //定义选择菜单执行
+  char input[N]; //定义发送字符串
+  char comeBackData[MAXSIZE];
+  while (isRun) {
+    memset(u, 0, sizeof(u));         //清理u结构体
+    memset(input, 0, sizeof(input)); //清理input
+    if (isMainmenu) {
+      mainmenu(bool isRun);//调用外层菜单
+    } else if (isAdmin_login(comeBackData)) {
+		adminMenu();
+    }else if(!isAdmin_login(comeBackData)){
+
+		normalUserMenu();
 	}
+  }
 
-	puts("连接服务器成功");
-	bool isloginpass=false;//是否成功登录
-	bool isRun=true;
-	while(isRun){
-		mainmenu(bool isRun);
-
-	}
-
-	 
-
-
-
-
-
-
-    return 0;
+  return 0;
 }
