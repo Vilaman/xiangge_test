fdefa217af4437b76b749093d0558b019f0c6c24
LiuZzhao <lz363567051@163.com>
2024-05-15 15:42:32 +0800

    [新增]:http_conn,webserver,线程池,定时器和更新一些其他文件

--- makedown/locker.h
+++ makedown/locker.h
@@ -8,17 +8,17 @@
 class sem {
 public:
     sem(int value = 0) {
-        if(sem_init(&m_sem, 0, value) != 0) {
+        if (sem_init(&m_sem, 0, value) != 0) {
             throw std::exception();
         }
     }
-    ~sem(){
+    ~sem() {
         sem_destroy(&m_sem);
     }
-    bool wait(){
+    bool wait() {
         return sem_wait(&m_sem) == 0;
     }
-    bool post(){
+    bool post() {
         return sem_post(&m_sem) == 0;
     }
 private:
@@ -27,21 +27,21 @@ private:
 
 class locker {
 public:
-    locker(){
-        if(pthread_mutex_init(&m_mutex, NULL) != 0){
+    locker() {
+        if (pthread_mutex_init(&m_mutex, NULL) != 0) {
             throw std::exception();
         }
     }
-    ~locker(){
+    ~locker() {
         pthread_mutex_destroy(&m_mutex);
     }
-    bool lock(){
+    bool lock() {
         return pthread_mutex_lock(&m_mutex);
     }
-    bool unlock(){
+    bool unlock() {
         return pthread_mutex_unlock(&m_mutex);
     }
-    pthread_mutex_t *get(){
+    pthread_mutex_t* get() {
         return &m_mutex;
     }
 private:
@@ -50,25 +50,25 @@ private:
 
 class cond {
 public:
-    cond(){
-        if(pthread_cond_init(&m_cond, NULL) != 0){
+    cond() {
+        if (pthread_cond_init(&m_cond, NULL) != 0) {
             throw std::exception();
         }
     }
-    ~cond(){
+    ~cond() {
         pthread_cond_destroy(&m_cond);
     }
-    bool wait(pthread_mutex_t *m_mutex) {
+    bool wait(pthread_mutex_t* m_mutex) {
         int ret = pthread_cond_wait(&m_cond, m_mutex);
         return ret == 0;
     }
-    bool timewait(pthread_mutex_t *m_mutex, struct timespec t){
+    bool timewait(pthread_mutex_t* m_mutex, struct timespec t) {
         return pthread_cond_timedwait(&m_cond, m_mutex, &t);
     }
-    bool signal(){
+    bool signal() {
         return pthread_cond_signal(&m_cond) == 0;
     }
-    bool broadcast(){
+    bool broadcast() {
         return pthread_cond_broadcast(&m_cond) == 0;
     }
 private:
@@ -76,3 +76,36 @@ private:
 };
 
 #endif
+
+/*
+// 生产者线程函数
+void* producer(void* arg) {
+    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
+    for (int item = 0; item < 20; item++) {
+        queue->push(item);
+        LOG_INFO("生产者产生数据i = %d", item);
+        usleep((rand() % 5 + 1) * 50000);
+    }
+    return nullptr;
+}
+
+// 消费者线程函数
+void* consumer(void* arg) {
+    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
+    for (int item = 0; item < 20; item++) {
+        queue->pop(item);
+        LOG_INFO("消费者处理数据i = %d", item);
+    }
+    return nullptr;
+}
+
+int main() {
+    block_queue<int> queue;
+    pthread_t tid_prod, tid_cons;
+    pthread_create(&tid_prod, nullptr, producer, &queue);
+    pthread_create(&tid_cons, nullptr, consumer, &queue);
+    pthread_join(tid_prod, nullptr);
+    pthread_join(tid_cons, nullptr);
+    return 0;
+}
+*/
--- makedown/main.cpp
+++ makedown/main.cpp
@@ -1,37 +1,36 @@
-﻿#include <iostream>
-#include <pthread.h>
-#include <unistd.h>
-#include "block_queue.h"
-#include "log.h"
-#include "sql_connection_pool.h"
+﻿#include "config.h"
 
-// 生产者线程函数
-void* producer(void* arg) {
-    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
-    for (int item = 0; item < 20; item++) {
-        queue->push(item);
-        LOG_INFO("生产者产生数据i = %d", item);
-        usleep((rand() % 5 + 1) * 50000);
-    }
-    return nullptr;
-}
+int main(int argc, char* argv[])
+{
+    // 需要修改的数据库信息,登录名,密码,库名
+    std::string user = "root";
+    std::string passwd = "root";
+    std::string databasename = "yourdb";
 
-// 消费者线程函数
-void* consumer(void* arg) {
-    block_queue<int>* queue = static_cast<block_queue<int>*>(arg);
-    for (int item = 0; item < 20; item++) {
-        queue->pop(item);
-        LOG_INFO("消费者处理数据i = %d", item);
-    }
-    return nullptr;
-}
+    // 命令行解析
+    Config config;
+    config.parse_arg(argc, argv);
 
-int main() {
-    block_queue<int> queue;
-    pthread_t tid_prod, tid_cons;
-    pthread_create(&tid_prod, nullptr, producer, &queue);
-    pthread_create(&tid_cons, nullptr, consumer, &queue);
-    pthread_join(tid_prod, nullptr);
-    pthread_join(tid_cons, nullptr);
+    WebServer server;
+
+    //初始化
+    server.init(config.PORT, user, passwd, databasename, config.LOGWrite,
+        config.OPT_LINGER, config.TRIGMode, config.sql_num, config.thread_num, config.actor_model);
+
+    //数据库
+    server.sql_pool();
+
+    //线程池
+    server.thread_pool();
+
+    //触发模式
+    server.trig_mode();
+
+    //监听
+    server.eventListen();
+
+    //运行
+    server.eventLoop();
+
     return 0;
 }
